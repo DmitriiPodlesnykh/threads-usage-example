811bee86281385d00e92ff57bbc0772639c3abc2
Dmitrii Podlesnykh <dmitrii.podlesnykh@gmail.com>
2022-12-17 22:24:46 +0000

    Add unit tests

---
+++ src/test/java/com/podlesnykh/dmitrii/threadsusageexample/MainTest.java
@@ -0,0 +1,12 @@
+package com.podlesnykh.dmitrii.threadsusageexample;
+
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    void main() {
+        System.out.println("Main class test worked");
+    }
+}
