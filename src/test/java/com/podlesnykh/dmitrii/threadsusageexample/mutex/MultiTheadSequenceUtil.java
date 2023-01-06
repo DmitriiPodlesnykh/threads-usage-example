ee66ab7eff6cdade3f2961e13f26eaf6168770a5
Dmitrii Podlesnykh <dmitrii.podlesnykh@gmail.com>
2023-01-06 12:33:26 +0000

    Refactoring Mutex test

--- src/test/java/com/podlesnykh/dmitrii/threadsusageexample/mutex/MultiTheadSequenceUtil.java
+++ src/test/java/com/podlesnykh/dmitrii/threadsusageexample/mutex/MultiTheadSequenceUtil.java
@@ -9,26 +9,35 @@ import java.util.stream.IntStream;
 
 public class MultiTheadSequenceUtil {
 
-    public Set<Integer> getUniqueSequences(SequenceGenerator sequenceGenerator,
-                                           int count) throws InterruptedException, ExecutionException {
-        ExecutorService executorService = Executors.newFixedThreadPool(3);
+    public static final int COUNT_ITERATIONS = 10_000;
 
-        Set<Integer> uniqueSequences = new LinkedHashSet<>();
-        List<Future<Integer>> futures = new ArrayList<>();
+    public Set<Integer> getUniqueSequences(final SequenceGenerator sequenceGenerator,
+                                           final int count) throws InterruptedException, ExecutionException {
+        final ExecutorService executorService = Executors.newFixedThreadPool(3);
+        final List<Future<Integer>> futures = prepareFutures(sequenceGenerator, count,  executorService);
+        final Set<Integer> uniqueSequences = getFuturesResults(futures);
+        executorService.awaitTermination(1, TimeUnit.SECONDS);
+        executorService.shutdown();
+        return uniqueSequences;
+    }
 
+    private List<Future<Integer>> prepareFutures(final SequenceGenerator sequenceGenerator,
+                                                 final int count,
+                                                 final ExecutorService executorService) {
+        final List<Future<Integer>> futures = new ArrayList<>();
+        IntStream.range(0, count).forEach(v -> {
+            Future<Integer> future = executorService.submit(sequenceGenerator::getNextSequence);
+            futures.add(future);
+        });
+        return futures;
+    }
 
-
-        IntStream.range(0, count)
-                .forEach(v -> {
-                    Future<Integer> future = executorService.submit(sequenceGenerator::getNextSequence);
-                    futures.add(future);
-                });
-
+    private Set<Integer> getFuturesResults(List<Future<Integer>> futures)
+            throws ExecutionException, InterruptedException {
+        final Set<Integer> uniqueSequences = new LinkedHashSet<>();
         for (Future<Integer> future : futures) {
             uniqueSequences.add(future.get());
         }
-        executorService.awaitTermination(1, TimeUnit.SECONDS);
-        executorService.shutdown();
         return uniqueSequences;
     }
 }
--- src/test/java/com/podlesnykh/dmitrii/threadsusageexample/mutex/SequenceGeneratorTest.java
+++ src/test/java/com/podlesnykh/dmitrii/threadsusageexample/mutex/SequenceGeneratorTest.java
@@ -1,20 +1,31 @@
 package com.podlesnykh.dmitrii.threadsusageexample.mutex;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Set;
 import java.util.concurrent.ExecutionException;
 
+import static com.podlesnykh.dmitrii.threadsusageexample.mutex.MultiTheadSequenceUtil.COUNT_ITERATIONS;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SequenceGeneratorTest {
 
+    private SequenceGenerator sut;
+
+    private MultiTheadSequenceUtil multiTheadSequenceUtil;
+
+    @BeforeEach
+    public void setUp() {
+        sut = new SequenceGenerator();
+        multiTheadSequenceUtil = new MultiTheadSequenceUtil();
+    }
+
     @Test
     void getNextSequence_incorrectCalculation() throws ExecutionException, InterruptedException {
-        int count = 10_000;
 
-        MultiTheadSequenceUtil multiTheadSequenceUtil = new MultiTheadSequenceUtil();
-        Set<Integer> integers = multiTheadSequenceUtil.getUniqueSequences(new SequenceGenerator(), count);
-        assertNotEquals(count, integers.size());
+        Set<Integer> integers = multiTheadSequenceUtil.getUniqueSequences(sut, COUNT_ITERATIONS);
+
+        assertNotEquals(COUNT_ITERATIONS, integers.size());
     }
 }
